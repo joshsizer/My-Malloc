96ac5bd53434a79d35c74f6e8acb379eafeca80c
Joshua Sizer <joshsizer@gmail.com>
2021-04-28 15:54:00 -0400

    updated readme and added comment to top of file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # my_malloc
 A custom implementation of C malloc/free function.
+
+I use a linked list to keep track of used/free
+blocks of memory. To allocate a user new memory, a
+first fit algorithm is used before requesting more
+memory from the OS. When possible, neighboring
+free blocks are coalesced into one in order to
+reduce external fragmentation.
--- mymalloc.c
+++ mymalloc.c
@@ -1,4 +1,14 @@
-// Created by Joshua Sizer
+/**
+ * Author: Joshua Sizer
+ *
+ * This is a custom implementation of malloc/free.
+ * I use a linked list to keep track of used/free
+ * blocks of memory. To allocate a user new
+ * memory, a first fit algorithm is used before
+ * requesting more memory from the OS. When possible,
+ * neighboring free blocks are coalesced into one
+ * in order to reduce external fragmentation.
+ */
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
